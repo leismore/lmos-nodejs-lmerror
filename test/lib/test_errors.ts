9cf244f0b4fbef6daba373a6aa1360dd3aec6302
Kyle Chine <kyle.chine@leismore.co>
2025-08-15 13:51:00 +1000

    test: add assertions for error properties (cause, response, previous) in test_errors function

--- test/lib/test_errors.ts
+++ test/lib/test_errors.ts
@@ -43,7 +43,10 @@ function test_errors():void
                 assert.ok(error instanceof LMError, String(error));
                 assert.strictEqual(error.name, 'LMError', error.name);
                 assert.strictEqual(error.message, value.message, error.message);
+                assert.strictEqual(error.cause, undefined, String(error.cause));
                 assert.deepStrictEqual(error.error, value, JSON.stringify(error.error));
+                assert.strictEqual(error.response, undefined, JSON.stringify(error.response));
+                assert.strictEqual(error.previous, undefined, String(error.previous));
                 assert.strictEqual(String(error), (
                     `${error.timestamp.toISOString()} / LMError`   + EOL +
                     `${error.error.message} (${error.error.code})` + EOL
