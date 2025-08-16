5ed23148775baad5e3b98fb2d6f46b2c0f60795c
Kyle Chine <kyle.chine@leismore.co>
2025-08-16 15:25:13 +1000

    style: adjust import formatting for consistency in test.ts

--- test/test.ts
+++ test/test.ts
@@ -1,7 +1,6 @@
-import { test_errors } from '@test/lib/test_errors.js';
+import { test_errors    } from '@test/lib/test_errors.js';
 import { test_responses } from '@test/lib/test_responses.js';
 import { test_preErrors } from '@test/lib/test_preErrors.js';
-
 
 test_errors();
 test_responses();
