524b7e2f3d1625bcbc070e03aa93341b5bd6a57b
Kyle Chine <kyle.chine@leismore.co>
2025-08-16 11:01:52 +1000

    test: enhance test_responses function with additional validation for LMError class and response handling - done

--- test/lib/test_responses.ts
+++ test/lib/test_responses.ts
@@ -5,7 +5,7 @@
 import { EOL }                  from 'os';
 import assert                   from 'node:assert/strict';
 import test                     from 'node:test';
-import { LMError }              from '@src/index.js';
+import { LMError, LMErrorResHeader } from '@src/index.js';
 import { ALL as PRIMITIVE_ALL } from '@leismore/lmos-nodejs-primitives';
 
 import {
@@ -13,28 +13,8 @@ import {
 } from '@test/lib/errors.js';
 
 import {
-    RESPONSE_EMPTY,
-    RESPONSE_WITHOUT_CODE_HEADERS_BODY,
-    RESPONSE_HEADERS_NOT_ARRAY,
-    RESPONSE_HEADERS_WITHOUT_NAME_VALUE,
-    RESPONSE_HEADERS_WITHOUT_NAME,
-    RESPONSE_HEADERS_WITHOUT_VALUE,
-    RESPONSE_CODE_NOT_STRING,
-    RESPONSE_CODE_EMPTY,
-    RESPONSE_CODE_2DIGITS,
-    RESPONSE_CODE_4DIGITS,
-    RESPONSE_CODE_NOT_DIGITS,
-    RESPONSE_HEADERS_NAME_NOT_STRING,
-    RESPONSE_HEADERS_VALUE_NOT_STRING,
-    RESPONSE_HEADERS_NAME_VALUE_NOT_STRING,
-    RESPONSE_HEADERS_NAME_EMPTY,
-    RESPONSE_HEADERS_VALUE_EMPTY,
-    RESPONSE_HEADERS_NAME_VALUE_EMPTY,
-    RESPONSE_VALID,
-    RESPONSE_VALID_CODE,
-    RESPONSE_VALID_CODE_HEADERS,
-    RESPONSE_VALID_CODE_HEADERS_EMPTY_ARRAY,
-    RESPONSE_VALID_CODE_BODY
+    VALID   as RESPONSE_VALID,
+    INVALID as RESPONSE_INVALID
 } from '@test/lib/responses.js';
 
 
@@ -69,43 +49,125 @@ function test_responses():void
             });
         }
 
-        // RESPONSE_EMPTY
-        await t.test(`LMErrorRes - ${JSON.stringify(RESPONSE_EMPTY)}`, () => {
-            assert.throws(() => {
-                // @ts-expect-error
-                new LMError(THE_VALID_ERROR, RESPONSE_EMPTY);
-            }, Error);
-        });
-
-        // RESPONSE_WITHOUT_CODE_HEADERS_BODY
-        await t.test(`LMErrorRes - ${JSON.stringify(RESPONSE_WITHOUT_CODE_HEADERS_BODY)}`,
-            () => {
+        // Invalid objects
+        for (const value of RESPONSE_INVALID)
+        {
+            await t.test(`LMErrorRes - ${JSON.stringify(value)}`, () => {
                 assert.throws(() => {
                     // @ts-expect-error
-                    new LMError(THE_VALID_ERROR, RESPONSE_WITHOUT_CODE_HEADERS_BODY);
+                    new LMError(THE_VALID_ERROR, value);
                 }, Error);
-            }
-        );
+            });
+        }
 
-        // RESPONSE_HEADERS_NOT_ARRAY
-        await t.test(`LMErrorRes - ${JSON.stringify(RESPONSE_HEADERS_NOT_ARRAY)}`,
-            () => {
-                assert.throws(() => {
-                    // @ts-expect-error
-                    new LMError(THE_VALID_ERROR, RESPONSE_HEADERS_NOT_ARRAY);
-                }, Error);
-            }
-        );
+        // Valid objects
+        for (const value of RESPONSE_VALID)
+        {
+            await t.test(`LMErrorRes - ${JSON.stringify(value)}`, () => {
 
-        // RESPONSE_HEADERS_WITHOUT_NAME_VALUE
-        await t.test(`LMErrorRes - ${JSON.stringify(RESPONSE_HEADERS_WITHOUT_NAME_VALUE)}`,
-            () => {
-                assert.throws(() => {
-                    // @ts-expect-error
-                    new LMError(THE_VALID_ERROR, RESPONSE_HEADERS_WITHOUT_NAME_VALUE);
-                }, Error);
-            }
-        );
+                // Normalising test cases
+                const statusCode = value.statusCode;
+                let headers:(LMErrorResHeader[]|undefined) = undefined;
+                let body:unknown = undefined;
+
+                if (  'headers' in value === false                        ||
+                    ( 'headers' in value && value.headers === undefined ) ||
+                    ( 'headers' in value            &&
+                       Array.isArray(value.headers) &&
+                       value.headers.length === 0      )
+                ) {
+                    headers = undefined;
+                } else {
+                    // @ts-ignore
+                    headers = value.headers;
+                }
+
+                if (   'body' in value === false ||
+                     ( 'body' in value && value.body === undefined )
+                )
+                {
+                    body = undefined;
+                } else {
+                    body = value.body;
+                }
+
+                // LMError instance
+                const error = new LMError(THE_VALID_ERROR, value);
+
+                // Testing JavaScript standard properties
+                assert.ok(error instanceof LMError, String(error));
+                assert.strictEqual(error.name, 'LMError', error.name);
+                assert.strictEqual(error.message, THE_VALID_ERROR.message, error.message);
+                assert.strictEqual(error.cause, undefined, String(error.cause));
+
+                // Testing LMError properties
+                assert.deepStrictEqual(
+                    error.error,
+                    THE_VALID_ERROR,
+                    JSON.stringify(error.error)
+                );
+
+                assert.strictEqual(
+                    error.response?.statusCode,
+                    statusCode,
+                    error.response?.statusCode
+                );
+
+                if (headers === undefined)
+                {
+                    assert.strictEqual(
+                        error.response.headers,
+                        undefined,
+                        JSON.stringify(error.response.headers)
+                    );
+                } else {
+                    assert.deepStrictEqual(
+                        error.response.headers,
+                        headers,
+                        JSON.stringify(error.response.headers)
+                    );
+                }
+
+                if (body === undefined)
+                {
+                    assert.strictEqual(
+                        error.response.body,
+                        undefined,
+                        String(error.response.body)
+                    );
+                } else {
+                    assert.deepStrictEqual(
+                        error.response.body,
+                        body,
+                        String(error.response.body)
+                    );
+                }
+
+                assert.strictEqual(error.previous, undefined, String(error.previous));
+
+                // LMError method - toString
+                assert.strictEqual(
+
+                    String(error),
+                    
+                    (
+                      ( `${error.timestamp.toISOString()} / LMError`           + EOL ) +
+                      ( `${THE_VALID_ERROR.message} (${THE_VALID_ERROR.code})` + EOL ) +
+
+                      ( `HTTP ${statusCode}`                                   + EOL ) +
+                      ( headers === undefined ? '' : ( headers?.map(
+                          ( header:LMErrorResHeader ) => {
+                            return `${header.name}: ${header.value}`
+                          }).join(EOL) + EOL )                                       ) +
+
+                      ( body === undefined ? '' : ( JSON.stringify(body) + EOL )     )
+                    ),
+
+                    String(error)
+                );
+
+            });
+        }
 
     });
 }
